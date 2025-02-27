8b012fa629b5a7d819c9a39cb899378721fe7c12
cparent1234 <117628931+cparent1234@users.noreply.github.com>
2025-02-27 12:50:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,25 @@
-const CACHE_NAME = 'weather-app-v1';
+const CACHE_NAME = 'karten-workout-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/app.js',
-    '/manifest.json'
+  './',
+  'index.html'
 ];
 
+// Einfache Installation - nur wesentliche Dateien cachen
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// Vereinfachte Fetch-Strategie
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
